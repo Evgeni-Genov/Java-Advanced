14a1973133e06b3747b79081f65f686ae88c2b39
Evgeni Genov <43596303+Evgeni-Genov@users.noreply.github.com>
2021-12-11 17:22:20 +0200

    Add files via upload

---
+++ Functional Programming/src/SumNumbers.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Function;
+
+public class SumNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] numbers = Arrays.stream(scanner.nextLine().split(", "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        Function<int[], String> getCountNumbers = arr ->String.format(
+                "Count = %d", arr.length);
+
+        System.out.println(getCountNumbers.apply(numbers));
+
+        Function<int[], String> getSumOfNumbers = arr -> String.format("Sum = %s", Arrays.stream(arr).sum());
+        System.out.println(getSumOfNumbers.apply(numbers));
+    }
+}
